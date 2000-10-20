4a6bb21a88baccb56d3f7f7d20e341d0c4c49ef5
Daniel Schmelzer <dschmelzer@users.sourceforge.net>
2000-10-20 01:22:35 +0000

    Initial Site CVS Commit

---
+++ htdocs/index.php
@@ -0,0 +1,71 @@
+<html>
+<head>
+<title>DTV -- TV &amp; Video Deinterlacer</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<style type="text/css">
+<!--
+p {  font-family: Arial, Helvetica, sans-serif; font-size: 10pt; font-style: normal; color: #666600; text-decoration: none}
+.bland {  text-decoration: none; font-family: Arial, Helvetica, sans-serif; font-size: 10pt; color: #222233}
+-->
+</style>
+</head>
+
+<body bgcolor="#FFFFFF" vlink="#CC0000">
+<font size="1"> 
+<table width="97%" border="0" cellspacing="0" cellpadding="0" height="97%" align="center">
+  <tr bgcolor="#FFFFFF"> 
+    <td height="3%">&nbsp;</td>
+    <td height="3%">&nbsp;</td>
+    <td height="3%" align="right" valign="middle"><font color="#FFFFFF"><font face="Verdana, Arial, Helvetica, sans-serif" size="2" color="#000000"><b>D 
+      E I N T E R L A C E R&nbsp;</b></font></font></td>
+  </tr>
+  <tr bgcolor="#FFCC00"> 
+    <td height="2" colspan="3"><img src="/dtv/images/yellowspacer.gif" width="100%" height="2" border="0"></td>
+  </tr>
+  <tr> 
+    <td width="33%" valign="top" height="30%"> 
+      <p>&nbsp;<a href="/dtv/about.htm" class="bland">About</a><br>
+        &nbsp;<a href="/dtv/Reviews.htm" class="bland">Reviews</a><br>
+        &nbsp;<a href="/dtv/screenshots.htm" class="bland">Screenshots</a><br>
+        &nbsp;<a href="/dtv/News.htm" class="bland">News</a><br>
+        &nbsp;<a href="/dtv/downloads.htm" class="bland">Download</a><br>
+        &nbsp;<a href="/dtv/Authors.htm" class="bland">Authors</a><br>
+        <a href="/dtv/Thanks.htm" class="bland">&nbsp;Thanks</a> </p>
+    </td>
+    <td rowspan="3" height="90%" bordercolor="#999900" align="center" valign="middle"><img src="/dtv/images/dtvlogotv.gif" width="275" height="240" alt="dTV Marquee"></td>
+    <td width="33%" height="30%" valign="top"> 
+      <div align="right"><font size="1"><a href="/dtv/developers.htm"><img src="/dtv/images/developers.jpg" width="177" height="160" border="0"></a></font> 
+      </div>
+    </td>
+  </tr>
+  <tr align="center" valign="middle"> 
+    <td height="30%">&nbsp;</td>
+    <td height="30%">&nbsp;</td>
+  </tr>
+  <tr> 
+    <td width="33%" height="30%">&nbsp; </td>
+    <td width="33%" valign="bottom" height="30%"><br>
+      <p align="right"> <a href="/dtv/installation.htm" class="bland">Setup</a>&nbsp;<br>
+        <a href="/dtv/keyboard.htm" class="bland">Keyboard Shortcuts</a>&nbsp;<br>
+        <a href="http://sourceforge.net/mail/?group_id=7420" class="bland">Mailing 
+        List&nbsp;</a><br>
+        <a href="/dtv/FAQ.htm" class="bland">FAQ</a>&nbsp;<br>
+        <a href="/dtv/developers.htm" class="bland">Developers</a>&nbsp;<br>
+        <a href="/dtv/Links.htm" class="bland">Links</a>&nbsp;</p>
+    </td>
+  </tr>
+  <tr bgcolor="#FFCC00"> 
+    <td align="left" height="2" valign="middle" colspan="3"><img src="/dtv/images/yellowspacer.gif" width="100%" height="2"></td>
+  </tr>
+  <tr bgcolor="#FFFFFF"> 
+    <td align="left" height="3%" valign="middle"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"> 
+      <font color="#000000"><b>&nbsp;T V &amp; V I D E O</b></font></font></td>
+    <td height="3%">&nbsp;</td>
+    <td height="3%">&nbsp;</td>
+  </tr>
+</table>
+</font> 
+<p>&nbsp;</p>
+<p><a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=7420&type=1" width="88" height="31" border="0" alt="SourceForge.net"></a></p>
+</body>
+</html>
