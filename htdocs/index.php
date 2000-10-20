cef60efbfe09716e4c6dde0009e046d600a27116
Daniel Schmelzer <dschmelzer@users.sourceforge.net>
2000-10-20 01:54:19 +0000

    Preparation for Others To Checkout

--- htdocs/index.php
+++ htdocs/index.php
@@ -20,21 +20,21 @@ p {  font-family: Arial, Helvetica, sans-serif; font-size: 10pt; font-style: nor
       E I N T E R L A C E R&nbsp;</b></font></font></td>
   </tr>
   <tr bgcolor="#FFCC00"> 
-    <td height="2" colspan="3"><img src="/dtv/images/yellowspacer.gif" width="100%" height="2" border="0"></td>
+    <td height="2" colspan="3"><img src="/images/yellowspacer.gif" width="100%" height="2" border="0"></td>
   </tr>
   <tr> 
     <td width="33%" valign="top" height="30%"> 
-      <p>&nbsp;<a href="/dtv/about.htm" class="bland">About</a><br>
-        &nbsp;<a href="/dtv/Reviews.htm" class="bland">Reviews</a><br>
-        &nbsp;<a href="/dtv/screenshots.htm" class="bland">Screenshots</a><br>
-        &nbsp;<a href="/dtv/News.htm" class="bland">News</a><br>
-        &nbsp;<a href="/dtv/downloads.htm" class="bland">Download</a><br>
-        &nbsp;<a href="/dtv/Authors.htm" class="bland">Authors</a><br>
-        <a href="/dtv/Thanks.htm" class="bland">&nbsp;Thanks</a> </p>
+      <p>&nbsp;<a href="/about.htm" class="bland">About</a><br>
+        &nbsp;<a href="/Reviews.htm" class="bland">Reviews</a><br>
+        &nbsp;<a href="/screenshots.htm" class="bland">Screenshots</a><br>
+        &nbsp;<a href="/News.htm" class="bland">News</a><br>
+        &nbsp;<a href="/downloads.htm" class="bland">Download</a><br>
+        &nbsp;<a href="/Authors.htm" class="bland">Authors</a><br>
+        <a href="/Thanks.htm" class="bland">&nbsp;Thanks</a> </p>
     </td>
-    <td rowspan="3" height="90%" bordercolor="#999900" align="center" valign="middle"><img src="/dtv/images/dtvlogotv.gif" width="275" height="240" alt="dTV Marquee"></td>
+    <td rowspan="3" height="90%" bordercolor="#999900" align="center" valign="middle"><img src="/images/dtvlogotv.gif" width="275" height="240" alt="dTV Marquee"></td>
     <td width="33%" height="30%" valign="top"> 
-      <div align="right"><font size="1"><a href="/dtv/developers.htm"><img src="/dtv/images/developers.jpg" width="177" height="160" border="0"></a></font> 
+      <div align="right"><font size="1"><a href="/developers.htm"><img src="/images/developers.jpg" width="177" height="160" border="0"></a></font> 
       </div>
     </td>
   </tr>
@@ -45,17 +45,17 @@ p {  font-family: Arial, Helvetica, sans-serif; font-size: 10pt; font-style: nor
   <tr> 
     <td width="33%" height="30%">&nbsp; </td>
     <td width="33%" valign="bottom" height="30%"><br>
-      <p align="right"> <a href="/dtv/installation.htm" class="bland">Setup</a>&nbsp;<br>
-        <a href="/dtv/keyboard.htm" class="bland">Keyboard Shortcuts</a>&nbsp;<br>
+      <p align="right"> <a href="/installation.htm" class="bland">Setup</a>&nbsp;<br>
+        <a href="/keyboard.htm" class="bland">Keyboard Shortcuts</a>&nbsp;<br>
         <a href="http://sourceforge.net/mail/?group_id=7420" class="bland">Mailing 
         List&nbsp;</a><br>
-        <a href="/dtv/FAQ.htm" class="bland">FAQ</a>&nbsp;<br>
-        <a href="/dtv/developers.htm" class="bland">Developers</a>&nbsp;<br>
-        <a href="/dtv/Links.htm" class="bland">Links</a>&nbsp;</p>
+        <a href="/FAQ.htm" class="bland">FAQ</a>&nbsp;<br>
+        <a href="/developers.htm" class="bland">Developers</a>&nbsp;<br>
+        <a href="/Links.htm" class="bland">Links</a>&nbsp;</p>
     </td>
   </tr>
   <tr bgcolor="#FFCC00"> 
-    <td align="left" height="2" valign="middle" colspan="3"><img src="/dtv/images/yellowspacer.gif" width="100%" height="2"></td>
+    <td align="left" height="2" valign="middle" colspan="3"><img src="/images/yellowspacer.gif" width="100%" height="2"></td>
   </tr>
   <tr bgcolor="#FFFFFF"> 
     <td align="left" height="3%" valign="middle"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"> 
