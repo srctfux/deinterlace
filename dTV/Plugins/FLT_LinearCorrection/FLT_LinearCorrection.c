49f8c505003a85fd336abc75bb724edf9c292bd5
Laurent Garnier <laurentg@users.sourceforge.net>
2001-05-09 20:42:55 +0000

    no message

--- dTV/Plugins/FLT_LinearCorrection/FLT_LinearCorrection.c
+++ dTV/Plugins/FLT_LinearCorrection/FLT_LinearCorrection.c
@@ -23,23 +23,27 @@
 #define	MAX_HEIGHT	576
 #define	MAX_WIDTH	768
 
+#define	Y_BLACK		16
+#define	UV_BLACK	128
+
 FILTER_METHOD LinearCorrMethod;
 
 typedef struct _BlendStruct
 {
-	int	pixel1;		// The first pixel to use to calculate luminance
-	int	coef1;		// Coef to apply to pixel1
-					// double value between 0 and 1 multiplied by 1000 to use integer
-	int	pixel2;		// The second pixel to use to calculate luminance
-	int	coef2;		// Coef to apply to pixel2
-					// double value between 0 and 1 multiplied by 1000 to use integer
-	int	pixelUV;	// Pixel to use to calculate color
+	short	pixel1Y;	// The first pixel to use to calculate luminance
+	short	pixel2Y;	// The second pixel to use to calculate luminance
+	short	pixel1UV;	// The first pixel to use to calculate color
+	short	pixel2UV;	// The second pixel to use to calculate color
+	short	coef1;		// Coef to apply to pixel1
+						// double value between 0 and 1 multiplied by 1000 to use integer
+	short	coef2;		// Coef to apply to pixel2
+						// double value between 0 and 1 multiplied by 1000 to use integer
 } BlendStruct;
 
 int PictureWidth = -1;
 int PictureHeight = -1;
 int NbPixelsPerLineTab[MAX_HEIGHT];
-BlendStruct LinearFilterTab[MAX_WIDTH+2][MAX_WIDTH];
+BlendStruct LinearFilterTab[MAX_WIDTH+1][MAX_WIDTH];
 BYTE TmpBuf[MAX_WIDTH*2];
 
 BOOL DoOnlyMasking = FALSE;
@@ -52,50 +56,97 @@ int MaskParam4 = 0;
 void UpdLinearFilterTables(int Width)
 {
 	int i, j;
-	int Start, End;
+	double Start, End;
 	double pos;
 	double pixel_before, pixel_after;
 
 	for (i=0 ; i<=Width ; i++)
 	{
-		Start = (Width - i) / 2;
+		Start = (double)(Width - i) / 2.0;
 		End = Start + i - 1;
 		for (j=0 ; j<Width ; j++)
 		{
-			if ((j < Start) || (j > End))
+			if ((i == 0) || ((double)j < Start) || ((double)j > End))
 			{
-				LinearFilterTab[i][j].pixel1 = -1;
-				LinearFilterTab[i][j].pixel2 = -1;
+				LinearFilterTab[i][j].pixel1Y = -1;
+				LinearFilterTab[i][j].pixel2Y = -1;
+				LinearFilterTab[i][j].pixel1UV = -1;
+				LinearFilterTab[i][j].pixel2UV = -1;
 				LinearFilterTab[i][j].coef1 = 0;
 				LinearFilterTab[i][j].coef2 = 0;
-				LinearFilterTab[i][j].pixelUV = -1;
 			}
 			else
 			{
 				if (i > 1)
-					pos = (double)(j - Start) / (double)(i - 1) * (double)(Width - 1);
+				{
+					pos = ((double)j - Start) / (double)i * (double)Width;
+				}
 				else
+				{
 					pos = (double)(Width - 1) / 2.0;
+				}
 				pixel_before = floor(pos);
 				pixel_after = ceil(pos);
-				LinearFilterTab[i][j].pixel1 = (int)pixel_before;
-				LinearFilterTab[i][j].pixel2 = (int)pixel_after;
+
+				LinearFilterTab[i][j].pixel1Y = (short)pixel_before;
+				LinearFilterTab[i][j].pixel2Y = (short)pixel_after;
+
+				if (LinearFilterTab[i][j].pixel1Y == LinearFilterTab[i][j].pixel2Y)
+				{
+					if ((j % 2) == (LinearFilterTab[i][j].pixel1Y % 2))
+					{
+						LinearFilterTab[i][j].pixel1UV = LinearFilterTab[i][j].pixel1Y;
+					}
+					else if ((j % 2) == 0)
+					{
+						LinearFilterTab[i][j].pixel1UV = LinearFilterTab[i][j].pixel1Y - 1;
+					}
+					else
+					{
+						LinearFilterTab[i][j].pixel1UV = LinearFilterTab[i][j].pixel1Y + 1;
+					}
+					LinearFilterTab[i][j].pixel2UV = LinearFilterTab[i][j].pixel1UV;
+				}
+				else
+				{
+					if ((j % 2) == (LinearFilterTab[i][j].pixel1Y % 2))
+					{
+						if ((j % 2) == 0)
+						{
+							LinearFilterTab[i][j].pixel1UV = LinearFilterTab[i][j].pixel1Y;
+							LinearFilterTab[i][j].pixel2UV = LinearFilterTab[i][j].pixel1Y;
+						}
+						else
+						{
+							LinearFilterTab[i][j].pixel1UV = LinearFilterTab[i][j].pixel1Y;
+							LinearFilterTab[i][j].pixel2UV = LinearFilterTab[i][j].pixel2Y + 1;
+						}
+					}
+					else if ((j % 2) == (LinearFilterTab[i][j].pixel2Y % 2))
+					{
+						if ((j % 2) == 0)
+						{
+							LinearFilterTab[i][j].pixel1UV = LinearFilterTab[i][j].pixel1Y - 1;
+							LinearFilterTab[i][j].pixel2UV = LinearFilterTab[i][j].pixel2Y;
+						}
+						else
+						{
+							LinearFilterTab[i][j].pixel1UV = LinearFilterTab[i][j].pixel2Y;
+							LinearFilterTab[i][j].pixel2UV = LinearFilterTab[i][j].pixel2Y;
+						}
+					}
+				}
+
 				if (pixel_before < pixel_after)
 				{
-					LinearFilterTab[i][j].coef1 = (int)ceil((pixel_after - pos) * 1000.0 - 0.5);
-					LinearFilterTab[i][j].coef2 = (int)ceil((pos - pixel_before) * 1000.0 - 0.5);
+					LinearFilterTab[i][j].coef1 = (short)ceil((pixel_after - pos) * 1000.0 - 0.5);
+					LinearFilterTab[i][j].coef2 = (short)ceil((pos - pixel_before) * 1000.0 - 0.5);
 				}
 				else
 				{
 					LinearFilterTab[i][j].coef1 = 500;
 					LinearFilterTab[i][j].coef2 = 500;
 				}
-				if ((j % 2) == (LinearFilterTab[i][j].pixel1 % 2))
-					LinearFilterTab[i][j].pixelUV = LinearFilterTab[i][j].pixel1;
-				else if ((j % 2) == (LinearFilterTab[i][j].pixel2 % 2))
-					LinearFilterTab[i][j].pixelUV = LinearFilterTab[i][j].pixel2;
-				else
-					LinearFilterTab[i][j].pixelUV = LinearFilterTab[i][j].pixel1 + 1;
 			}
 		}
 	}
@@ -106,6 +157,7 @@ void UpdNbPixelsPerLineTable(int Height, int Width)
 	int x;
 	double val1, val2, val3, val4;
 	double a, b;
+	double val;
 
 	switch (MaskType)
 	{
@@ -116,7 +168,8 @@ void UpdNbPixelsPerLineTable(int Height, int Width)
 		a = (val2 - b) / (double)(Height - 1);
 		for (x=0 ; x<Height ; x++)
 		{
-			NbPixelsPerLineTab[x] = (int)ceil(a * x + b - 0.5);
+			val = a * x + b;
+			NbPixelsPerLineTab[x] = (int)ceil(val - 0.5);
 		}
 		break;
 
@@ -129,20 +182,23 @@ void UpdNbPixelsPerLineTable(int Height, int Width)
 		a = (val2 - b) / val4;
 		for (x=0 ; x<(int)val4 ; x++)
 		{
-			NbPixelsPerLineTab[x] = (int)ceil(a * x + b - 0.5);
+			val = a * x + b;
+			NbPixelsPerLineTab[x] = (int)ceil(val - 0.5);
 		}
 		a = (val3 - val2) / ((double)(Height - 1) - val4);
 		b = val3 - a * (double)(Height - 1);
 		for (x=(int)val4 ; x<Height ; x++)
 		{
-			NbPixelsPerLineTab[x] = (int)ceil(a * x + b - 0.5);
+			val = a * x + b;
+			NbPixelsPerLineTab[x] = (int)ceil(val - 0.5);
 		}
 		break;
 
 	default:
 		for (x=0 ; x<Height ; x++)
 		{
-			NbPixelsPerLineTab[x] = Width;
+			val = (double)Width;
+			NbPixelsPerLineTab[x] = (int)ceil(val - 0.5);
 		}
 		break;
 	}
@@ -154,20 +210,20 @@ void ApplyLinearFilter(BYTE* pLine, int NewWidth, MEMCPY_FUNC *pCopy)
 
 	for (i=0 ; i<PictureWidth ; i++)
 	{
-		if (LinearFilterTab[NewWidth][i].pixel1 == -1)
+		if (LinearFilterTab[NewWidth][i].pixel1Y == -1)
 		{
 			// Color the pixel in black
 			if (!DoOnlyMasking)
 			{
 				// Build temporary new line
-				TmpBuf[i*2] = 16;
-				TmpBuf[i*2+1] = 128;
+				TmpBuf[i*2] = Y_BLACK;
+				TmpBuf[i*2+1] = UV_BLACK;
 			}
 			else
 			{
 				// Modify line
-				pLine[i*2] = 16;
-				pLine[i*2+1] = 128;
+				pLine[i*2] = Y_BLACK;
+				pLine[i*2+1] = UV_BLACK;
 			}
 		}
 		else
@@ -175,8 +231,9 @@ void ApplyLinearFilter(BYTE* pLine, int NewWidth, MEMCPY_FUNC *pCopy)
 			if (!DoOnlyMasking)
 			{
 				// Build temporary new line
-				TmpBuf[i*2] = (pLine[LinearFilterTab[NewWidth][i].pixel1*2] * LinearFilterTab[NewWidth][i].coef1 + pLine[LinearFilterTab[NewWidth][i].pixel2*2] * LinearFilterTab[NewWidth][i].coef2) / 1000;
-				TmpBuf[i*2+1] = pLine[LinearFilterTab[NewWidth][i].pixelUV*2+1];
+				TmpBuf[i*2] = (pLine[LinearFilterTab[NewWidth][i].pixel1Y*2] * LinearFilterTab[NewWidth][i].coef1 + pLine[LinearFilterTab[NewWidth][i].pixel2Y*2] * LinearFilterTab[NewWidth][i].coef2) / 1000;
+//				TmpBuf[i*2+1] = pLine[LinearFilterTab[NewWidth][i].pixel1UV*2+1];
+				TmpBuf[i*2+1] = (pLine[LinearFilterTab[NewWidth][i].pixel1UV*2+1] * LinearFilterTab[NewWidth][i].coef1 + pLine[LinearFilterTab[NewWidth][i].pixel2UV*2+1] * LinearFilterTab[NewWidth][i].coef2) / 1000;
 			}
 		}
 	}
@@ -193,14 +250,18 @@ BOOL LinearCorrection(DEINTERLACE_INFO *info)
 	int i;
 
 	if (info->Overlay == NULL)
+	{
 		return FALSE;
+	}
 
 	// If there is a change concerning the height or the width of the picture
 	if ((info->FrameWidth != PictureWidth) || (info->FrameHeight != PictureHeight))
 	{
 		// Verify that the filter can manage this size of picture
 		if ((info->FrameWidth > MAX_WIDTH) || (info->FrameHeight > MAX_HEIGHT))
+		{
 			return FALSE;
+		}
 
 		// Update the internal tables of the filter
 		UpdNbPixelsPerLineTable(info->FrameHeight, info->FrameWidth);
