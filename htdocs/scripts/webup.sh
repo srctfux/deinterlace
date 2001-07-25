d98a81461d77db2b1ab043d81d3277c02466823f
Daniel Schmelzer <dschmelzer@users.sourceforge.net>
2001-07-25 00:00:54 +0000

    Portuguese FAQ clean up

--- htdocs/scripts/webup.sh
+++ htdocs/scripts/webup.sh
@@ -55,7 +55,7 @@ rm -R -f htdocs/readme
 rm -R -f htdocs/screenshots
 rm -R -f htdocs/setup
 rm -R -f htdocs/scripts
-
+rm -R -f htdocs/portuguese
 
 #Check out the htdocs module from the dScaler CVS using pserver.
 #You will need to log in to pserver once; after that, you should be fine, unless the CVS
