6155a0c7345e832efd6c841a0d8e90c3b1e38ade
Torbjörn Jansson <tobbej@users.sourceforge.net>
2001-09-19 17:55:24 +0000

    forgot to add comment block at begining of file

--- DIDMO/DIDMO/DIDMOWrapper.h
+++ DIDMO/DIDMO/DIDMOWrapper.h
@@ -1,4 +1,31 @@
-// DIDMOWrapper.h : Declaration of the CDIDMOWrapper
+/////////////////////////////////////////////////////////////////////////////
+// $Id: DIDMOWrapper.h,v 1.2 2001-09-19 17:55:24 tobbej Exp $
+/////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2001 Torbjörn Jansson.  All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+//
+//  This file is subject to the terms of the GNU General Public License as
+//  published by the Free Software Foundation.  A copy of this license is
+//  included with this software distribution in the file COPYING.  If you
+//  do not have a copy, you may obtain a copy by writing to the Free
+//  Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//  This software is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details
+/////////////////////////////////////////////////////////////////////////////
+// Change Log
+//
+// Date          Developer             Changes
+//
+//
+/////////////////////////////////////////////////////////////////////////////
+// CVS Log
+//
+// $Log: not supported by cvs2svn $
+//
+//////////////////////////////////////////////////////////////////////////////
 
 #ifndef __DIDMOWRAPPER_H_
 #define __DIDMOWRAPPER_H_
