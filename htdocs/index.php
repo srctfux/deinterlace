bf87f3a5e1b81885053ed174373c5c44abc9af2a
John Adcock <adcockj@users.sourceforge.net>
2001-11-15 12:26:07 +0000

    Added Filter page and put link on home page

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,7 +1,9 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 3.2//EN">
 <html>
 <head>
 <title>DScaler - TV &amp; Video Deinterlacer</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta http-equiv="Content-Type" content=
+"text/html; charset=iso-8859-1">
 <style type="text/css">
 <!--
 p {  font-family: Arial, Helvetica, sans-serif; font-size: 10pt; font-style: normal; color: #666600; text-decoration: none}
@@ -9,63 +11,93 @@ p {  font-family: Arial, Helvetica, sans-serif; font-size: 10pt; font-style: nor
 -->
 </style>
 </head>
-
 <body bgcolor="#FFFFFF" vlink="#CC0000">
-<font size="1"> 
-<table width="97%" border="0" cellspacing="0" cellpadding="0" height="97%" align="center">
-  <tr bgcolor="#FFFFFF"> 
-    <td height="3%">&nbsp;</td>
-    <td height="3%">&nbsp;</td>
-    <td height="3%" align="right" valign="middle"><font color="#FFFFFF"><font face="Verdana, Arial, Helvetica, sans-serif" size="2" color="#000000"><b>D 
-      E I N T E R L A C E R&nbsp;</b></font></font></td>
-  </tr>
-  <tr bgcolor="#FFCC00"> 
-    <td height="2" colspan="3"><img src="images/yellowspacer.gif" width="100%" height="2" border="0"></td>
-  </tr>
-  <tr> 
-    <td width="33%" valign="top" height="30%"> 
-      <p>&nbsp;<a href="about.htm" class="bland">About</a><br>
-        &nbsp;<a href="reviews/index.html" class="bland">What they say about it</a><br>
-        &nbsp;<a href="screenshots/index.htm" class="bland">Screenshots</a><br>
-        &nbsp;<a href="downloads.htm" class="bland">News & Download</a><br>
-        &nbsp;<a href="Authors.htm" class="bland">Authors</a><br>
-        <a href="Thanks.htm" class="bland">&nbsp;Thanks</a> </p>
-    </td>
-    <td rowspan="3" height="90%" bordercolor="#999900" align="center" valign="middle"><img src="images/dscalerlogobig.jpg" alt="DScaler Marquee"></td>
-    <td width="33%" height="30%" valign="top"> 
-      <div align="right"><font size="1"><a href="/developers.htm"><img src="images/developers.jpg" width="177" height="160" border="0"></a></font> 
-      </div>
-    </td>
-  </tr>
-  <tr align="center" valign="middle"> 
-    <td height="30%">&nbsp;</td>
-    <td height="30%">&nbsp;</td>
-  </tr>
-  <tr> 
-    <td width="33%" height="30%">&nbsp; </td>
-    <td width="33%" valign="bottom" height="30%"><br>
-      <p align="right"> <a href="setup/index.htm" class="bland">Setup</a>&nbsp;<br>
-        <a href="keyboard.htm" class="bland">Keyboard Shortcuts</a>&nbsp;<br>
-        <a href="http://sourceforge.net/mail/?group_id=7420" class="bland">Mailing 
-        List&nbsp;</a><br>
-        <a href="FAQ.htm" class="bland">FAQ</a>&nbsp;<br>
-        <a href="user_support.htm" class="bland">User Support</a>&nbsp;<br>
-        <a href="developers.htm" class="bland">Developers</a>&nbsp;<br>
-        <a href="Links.htm" class="bland">Links</a>&nbsp;</p>
-    </td>
-  </tr>
-  <tr bgcolor="#FFCC00"> 
-    <td align="left" height="2" valign="middle" colspan="3"><img src="images/yellowspacer.gif" width="100%" height="2"></td>
-  </tr>
-  <tr bgcolor="#FFFFFF"> 
-    <td align="left" height="3%" valign="middle"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"> 
-      <font color="#000000"><b>&nbsp;T V &amp; V I D E O</b></font></font></td>
-    <td height="3%">&nbsp;</td>
-    <td height="3%">&nbsp;</td>
-  </tr>
+
+
+<table width="97%" border="0" cellspacing="0" cellpadding="0"
+height="97%" align="center">
+<tr bgcolor="#FFFFFF">
+<td height="3%"> </td>
+<td height="3%"> </td>
+<td height="3%" align="right" valign="middle"><font color=
+"#FFFFFF"><font face="Verdana, Arial, Helvetica, sans-serif"
+size="2" color="#000000"><b>D E I N T E R L A C E
+R </b></font></font> </td>
+</tr>
+
+<tr bgcolor="#FFCC00">
+<td height="2" colspan="3"><img src="images/yellowspacer.gif"
+width="100%" height="2" border="0"></td>
+</tr>
+
+<tr>
+<td width="33%" valign="top" height="30%">
+<p><a href="about.htm" class="bland">About</a><br>
+ <a href="reviews/index.html" class="bland">What they say about
+DScaler</a><br>
+ <a href="screenshots/index.htm" class=
+"bland">Screenshots</a><br>
+ <a href="downloads.htm" class="bland">DScaler News &
+Download</a><br>
+ <a href="Filter.htm" class="bland">DirectShow Filter</a><br>
+ <a href="Authors.htm" class="bland">Authors</a><br>
+ <a href="Thanks.htm" class="bland">Thanks</a></p>
+</td>
+<td rowspan="3" height="90%" bordercolor="#999900" align="center"
+valign="middle"><img src="images/dscalerlogobig.jpg" alt=
+"DScaler Marquee"></td>
+<td width="33%" height="30%" valign="top">
+<div align="right"><a href="/developers.htm"><font size="1"><img
+src="images/developers.jpg" width="177" height="160" border=
+"0"></font></a></div>
+</td>
+</tr>
+
+<tr align="center" valign="middle">
+<td height="30%"> </td>
+<td height="30%"> </td>
+</tr>
+
+<tr>
+<td width="33%" height="30%"></td>
+<td width="33%" valign="bottom" height="30%"><br>
+ 
+
+<p align="right"><a href="setup/index.htm" class=
+"bland">Setup</a><br>
+ <a href="keyboard.htm" class="bland">Keyboard Shortcuts</a><br>
+ <a href="http://sourceforge.net/mail/?group_id=7420" class=
+"bland">Mailing List</a><br>
+ <a href="FAQ.htm" class="bland">FAQ</a><br>
+ <a href="user_support.htm" class="bland">User Support</a><br>
+ <a href="developers.htm" class="bland">Developers</a><br>
+ <a href="http://sourceforge.net/projects/deinterlace/" class=
+"bland">SourceForge.net Project Page</a><br>
+ <a href="Links.htm" class="bland">Links</a></p>
+</td>
+</tr>
+
+<tr bgcolor="#FFCC00">
+<td align="left" height="2" valign="middle" colspan="3"><img src=
+"images/yellowspacer.gif" width="100%" height="2"></td>
+</tr>
+
+<tr>
+<td align="left" height="2" valign="middle" colspan="3"> </td>
+</tr>
+
+<tr bgcolor="#FFFFFF">
+<td align="left" height="3%" valign="middle"><font face=
+"Verdana, Arial, Helvetica, sans-serif" size="2"><font color=
+"#000000"><b> T V &amp; V I D E O</b></font></font></td>
+<td height="3%"> </td>
+<td height="3%" align="right">
+<p><a href="http://sourceforge.net"><img src=
+"http://sourceforge.net/sflogo.php?group_id=7420&type=1" width=
+"88" height="31" border="0" alt="SourceForge.net"></a></p>
+</td>
+</tr>
 </table>
-</font> 
-<p>&nbsp;</p>
-<p><a href="http://sourceforge.net"><img src="http://sourceforge.net/sflogo.php?group_id=7420&type=1" width="88" height="31" border="0" alt="SourceForge.net"></a></p>
 </body>
 </html>
+
