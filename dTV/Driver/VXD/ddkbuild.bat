5f97751b8b6bd776e78ea58983f4a7103e957768
John Adcock <adcockj@users.sourceforge.net>
2009-02-18 08:38:06 +0000

    Moved remotely

--- dTV/Driver/VXD/ddkbuild.bat
+++
@@ -1,9 +0,0 @@
-pushd .
-call %1\bin\setenv.bat %1 free
-echo %BASEDIR%
-popd
-if "%2" == "debug" set Debug=1
-if not exist Debug md Debug
-if not exist Release md Release
-if "%3" == "clean" nmake clean
-nmake
